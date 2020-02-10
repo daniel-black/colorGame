e62fbefbc8e2a061ffbb62f5f2d31ceeb7a5feb3
Daniel Black <drblack651@gmail.com>
2020-02-09 21:46:57 -0600

    see previous commit comment

--- colorGame.js
+++ colorGame.js
@@ -25,7 +25,7 @@ for (var i = 0; i < squares.length; i++) {
 		if(clickedColor === pickedColor) {
 			alert("CORRECT");
 		} else {
-			alert("wrong");
+			this.style.backgroundColor = "#232323";
 		}
 	});
 }
